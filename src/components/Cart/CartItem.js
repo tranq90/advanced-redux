971d58dc8650ef6fcca4e1aca1ef76c8d3d119bf
Quan Tran <tran.q90@gmail.com>
2021-04-26 21:06:07 -0700

    fetching and posting data

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,12 +1,18 @@
-import classes from "./CartItem.module.css";
-import { useDispatch } from "react-redux";
-import { cartActions } from "../../store/cart-slice";
+import { useDispatch } from 'react-redux';
+
+import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price, id } = props.item;
   const dispatch = useDispatch();
 
-  const addToCartHandler = () => {
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
     dispatch(
       cartActions.addItemToCart({
         id,
@@ -16,16 +22,12 @@ const CartItem = (props) => {
     );
   };
 
-  const removeFromCartHandler = () => {
-    dispatch(cartActions.removeItemFromCart(id));
-  };
-
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{" "}
+          ${total.toFixed(2)}{' '}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -34,8 +36,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button onClick={removeFromCartHandler}>-</button>
-          <button onClick={addToCartHandler}>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,18 +1,18 @@
-import ProductItem from "./ProductItem";
-import classes from "./Products.module.css";
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
 
 const DUMMY_PRODUCTS = [
   {
-    id: "p1",
+    id: 'p1',
     price: 6,
-    title: "my first book",
-    description: "the first book i ever wrote",
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
   },
   {
-    id: "p2",
-    price: 7,
-    title: "my second book",
-    description: "the second book i ever wrote",
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
   },
 ];
 
